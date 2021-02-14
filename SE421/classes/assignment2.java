6c48fd83f49953cd94b40ecd101a06a10d6ce4d7
karolord <karokodo123@gmail.com>
2021-02-14 15:08:29 +0300

    commit

---
+++ SE421/classes/assignment2.java
@@ -0,0 +1,25 @@
+package SE421.classes;
+
+public class assignment2 {
+   public static void main(String[] args) {
+       
+   } 
+
+
+
+   /**
+    * Innerassignment2
+    */
+   public class  Dinput{
+        double value;
+    }
+
+    public void addition() {
+        
+    }
+
+    public void () {
+        
+    }
+
+}
