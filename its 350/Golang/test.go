1801510a932f44271d142f685d2fdea4118eaec4
karolord <karokodo123@gmail.com>
2021-02-18 15:07:58 +0300

    commit

---
+++ its 350/Golang/test.go
@@ -0,0 +1,18 @@
+package main	
+imoort {
+	"fmt"
+}
+
+func main() {
+	var x int = 0 // 1 itme unit
+
+	for i := 0; i < n + 1; i++{
+		if i%2 == 0{
+		}
+				fmt.println("yeye")			
+	}
+
+}
+
+// time complexity is T(n) = 1 time unit
+ func
