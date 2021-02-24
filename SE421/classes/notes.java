70a130f631082088cf6a045cd1c67a6ce4c5bcba
karolord <karokodo123@gmail.com>
2021-02-24 12:37:41 +0300

    commit

---
+++ SE421/classes/notes.java
@@ -0,0 +1,17 @@
+package SE421.classes;
+
+import java.util.Scanner;
+
+/**
+ * notes
+ */
+public class notes {
+
+    public static void main(String[] args) {
+        System.out.println("AUIS"); 
+    classs yes = new classs();
+    yes.cID = 5;
+    yes.printID();       
+// the relation betweem main and classs is tightly coupled
+    }        
+}
