db5e5ef5829083ebac64117d7d9bc96a567f8ea4
karolord <karokodo123@gmail.com>
2021-03-03 00:01:36 +0300

    commit

---
+++ its 350/Assignment-1/stack.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+var stack [5]int
+var counter int
+
+func main() {
+	push(5)
+	fmt.Println(stack[0])
+	fmt.Println(counter)
+
+}
+
+func push(x int) {
+	stack[counter] = x
+	counter++
+}
+
+func pop() int {
+	if counter == 0 {
+		return 0, errors.new("stack is empty cannot pop")
+	} else {
+		var x = stack[counter-1]
+		stack[counter-1] = 0
+		counter--
+		return x, nil
+	}
+}
