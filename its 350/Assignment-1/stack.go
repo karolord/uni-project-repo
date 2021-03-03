76566e14b47d2feec1211cb5cf1e17bbfe20c7b9
karolord <karokodo123@gmail.com>
2021-03-03 05:43:27 +0300

    commit

--- its 350/Assignment-1/stack.go
+++ its 350/Assignment-1/stack.go
@@ -1,16 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 )
 
 var stack [5]int
 var counter int
 
 func main() {
-	push(5)
-	fmt.Println(stack[0])
-	fmt.Println(counter)
 
 }
 
@@ -21,11 +18,17 @@ func push(x int) {
 
 func pop() (int, error) {
 	if counter == 0 {
-		return 0, errors.new("stack is empty cannot pop")
+		return 0, errors.New("stack is empty cannot pop")
 	} else {
 		var x = stack[counter-1]
 		stack[counter-1] = 0
 		counter--
 		return x, nil
 	}
+}
+func maxGPAs() {
+	var maxGPA int
+	for i := 5; i > 0; i-- {
+	}
+
 }
