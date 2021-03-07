e93e6cfd0127282398b48cbfaf792c4bc13454bd
karolord <karokodo123@gmail.com>
2021-03-07 11:24:54 +0300

    commit

--- its 350/Assignment-1/stack.go
+++ its 350/Assignment-1/stack.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-
-	var x student
-	add(x)
-
-	
-	type student struct {
+	var n int32
+	fmt.Scanf(&n)
+	var arr [n]student
 }
 
+type student struct {
 	ID int
 }
 
-func add(s student) {
+func add(s *student) {
 	s.ID = 5
 }
