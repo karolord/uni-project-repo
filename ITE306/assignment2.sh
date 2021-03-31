5ba5f74b32409a6693587bd2b2a1fca0462bd9ea
karolord <karokodo123@gmail.com>
2021-03-31 20:25:34 +0300

    commit

---
+++ ITE306/assignment2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Karo Rasool kk19046@auis.edu.krd
+# Kosar Aziz kn18011@auis.edu.krd
+#Requirment 1
+mkdir -p /tmp/auis
+man renice > /tmp/auis/help-exported.txt
+
+
+#requirement2
+alias ite306="yes AUIS"
+
+
+#requirement3
+echo "Karo Rasool and Kosar Aziz" > ite306-$HOSTNAME.txt
+
+#requirement4
+cd /tmp && ls -ali | wc -l
+
