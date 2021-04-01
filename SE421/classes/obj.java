ed6340542e3515b4229e092ee0fb26759467216c
karolord <karokodo123@gmail.com>
2021-04-01 16:13:40 +0300

    commit

---
+++ SE421/classes/obj.java
@@ -0,0 +1,18 @@
+package SE421.classes;
+
+/**
+  anonymous classes in java
+  run time polymorphism 
+  run time structures
+ **/
+public class obj {
+    public static void main(String[] args) {
+        Listener l = new Listener(){
+            
+        }   ;
+    }
+    
+}
+interface Listener {
+
+}
