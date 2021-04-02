9f2a56e2298c1b93d7ecdb119f0d66ed621cc822
karolord <karokodo123@gmail.com>
2021-04-02 20:59:48 +0300

    commit

--- its 350/Assignment-2/Assignment2.go
+++ its 350/Assignment-2/Assignment2.go
@@ -1,11 +1,41 @@
 package main
 
 import "fmt"
+var slice []int
+var queue []int
 
 func main() {
+	
+}
+func enqueue(x int){
+	slice = append(slice,x)
+}
+func dequeue() int{
+	value := slice[0]
+	slice = slice[1:]
+	return value
 }
 
 func Requirement1() {
-	fmt.Println("sup kosar")
+
+}
+
+func Requirement2() {
+
+}
+
+func Requirement3() {
+
+}
+func Requirement4() {
+
+}
+func Requirement5() {
+
+}
+func Requirement6() {
+
+}
+func Requirement7() {
 
 }
