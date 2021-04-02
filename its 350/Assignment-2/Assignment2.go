015161f486620928b2420c072b19ec3f6d78e029
karolord <karokodo123@gmail.com>
2021-04-02 23:46:39 +0300

    commit

--- its 350/Assignment-2/Assignment2.go
+++ its 350/Assignment-2/Assignment2.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 var slice []int
 var queue []int
-var slicecounter int
 var counter int
+var slicecounter int
 var x int
 func main() {
 	Requirement1()
-	fmt.Println(slice)
-	fmt.Println(mergeSort(slice))
+	Requirement4()
+
 }
 func enqueue(x int){
 	queue[counter] = x
+	counter++ 
 }
 func dequeue() int{
 	value := queue[0]
@@ -25,7 +26,7 @@ func Requirement1() {
 	fmt.Scanln(&x)
 	slice = make([]int,x,x)
 	queue = make([]int,x,x)
-	
+
 	var grade int
 	for i := 0; i < x; i++ {
 		fmt.Println("Enter the grade:")
@@ -43,58 +44,55 @@ func Requirement1() {
 
 func Requirement2(grade int) {
 	slice[slicecounter] = grade
-	counter++
+	slicecounter++
 }
+
 func Requirement3(grade int) {
 	enqueue(grade)
 }
-
-func mergeSort(items []int) []int {
-    var num = len(items)
-     
-    if num == 1 {
-        return items
-    }
-     
-    middle := int(num / 2)
-    var (
-        left = make([]int, middle)
-        right = make([]int, num-middle)
-    )
-    for i := 0; i < num; i++ {
-        if i < middle {
-            left[i] = items[i]
-        } else {
-            right[i-middle] = items[i]
-        }
-    }
-     
-    return merge(mergeSort(left), mergeSort(right))
+func Requirement4() {
+	copyslice := slice
+	fmt.Println(mergesort(copyslice))
 }
- 
-func merge(left, right []int) (result []int) {
-    result = make([]int, len(left) + len(right))
-     
-    i := 0
-    for len(left) > 0 && len(right) > 0 {
-        if left[0] < right[0] {
-            result[i] = left[0]
-            left = left[1:]
-        } else {
-            result[i] = right[0]
-            right = right[1:]
-        }
-        i++
-    }
-     
-    for j := 0; j < len(left); j++ {
-        result[i] = left[j]
-        i++
-    }
-    for j := 0; j < len(right); j++ {
-        result[i] = right[j]
-        i++
-    }
-     
-    return
+func merge(left []int, right []int) []int {
+	result := make([]int,len(right)+len(left))
+	i := 0
+	j := 0
+	z := 0
+	for z < len(result){
+		if i > len(left)-1 && j < len(right){
+			result[z] = right[j]
+			z++
+			j++
+		} else if i < len(left) && j > len(right)-1 {
+			result[z] = left[i]
+			z++
+			i++
+		} else if left[i] > right[j] {
+			result[z] = left[i]
+			z++
+			i++
+		} else {
+			result[z] = right[j]
+			z++
+			j++
+		}
+	}
+	return result 
+}
+func mergesort(input []int) []int  {
+	if len(input) == 1{
+		return input
+	}
+	mid := len(input)/2
+	left := input[:mid]
+	right := input[mid:]
+	return merge(mergesort(left), mergesort(right))
+}
+func insertionsort(input []int){
+	
+	for i := 0; i < x; i++{
+
+	}
+	
 }
