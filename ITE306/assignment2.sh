4d0aa7baac61fa77da5caae6e897eb71c49d6ad0
karolord <karokodo123@gmail.com>
2021-04-03 14:56:11 +0300

    commit

--- ITE306/assignment2.sh
+++ ITE306/assignment2.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 # Karo Rasool kk19046@auis.edu.krd
 # Kosar Aziz kn18011@auis.edu.krd
-#Requirment 1
+# Requirement 1
 mkdir -p /tmp/auis
 man renice > /tmp/auis/help-exported.txt
 
 
-#requirement2
+# Requirement 2
 alias ite306="yes AUIS"
 
 
-#requirement3
+# Requirement 3
 echo "Karo Rasool and Kosar Aziz" > /home/ite306-$HOSTNAME.txt
 
-#requirement4
+# Requirement 4
 sudo find /tmp -type f | wc -l
 # citation find manual
 
-#requirement 5
+# Requirement 5
 echo $1 $2 $3 $4 $5 $6 $7 $8 $9
 var=$(echo $1 $2 $3 $4 $5 $6 $7 $8 $9 | wc -w)
 if [[ $var -lt 3 ]]
@@ -25,7 +25,7 @@ then
 echo Error
 exit
 fi
-#requirement6
-v1=$(ps aux | awk 'FNR == 2 {print $9}')
-V2=$(date -d "$v1 today + 360 minutes" +'%H:%M')
+# Requirement 6
+V1=$(ps aux | awk 'FNR == 2 {print $9}')
+V2=$(date -d "$V1 today + 360 minutes" +'%H:%M')
 shutdown $V2
