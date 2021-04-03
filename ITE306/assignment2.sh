eed1228b0694e4046fcccc6663e8311846358659
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-04-03 15:26:03 +0300

    Update assignment2.sh

--- ITE306/assignment2.sh
+++ ITE306/assignment2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Karo Rasool kk19046@auis.edu.krd
 # Kosar Aziz kn18011@auis.edu.krd
+
 # Requirement 1
 mkdir -p /tmp/auis
 man renice > /tmp/auis/help-exported.txt
@@ -13,9 +14,11 @@ alias ite306="yes AUIS"
 # Requirement 3
 echo "Karo Rasool and Kosar Aziz" > /home/ite306-$HOSTNAME.txt
 
+
 # Requirement 4
 sudo find /tmp -type f | wc -l
 # citation find manual
+
 
 # Requirement 5
 echo $1 $2 $3 $4 $5 $6 $7 $8 $9
@@ -25,6 +28,8 @@ then
 echo Error
 exit
 fi
+
+
 # Requirement 6
 V1=$(ps aux | awk 'FNR == 2 {print $9}')
 V2=$(date -d "$V1 today + 360 minutes" +'%H:%M')
