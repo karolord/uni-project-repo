8d90b6167418e3d476cce7921426647bf2d33f5c
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-04-05 03:42:18 +0300

    Update Assignmentfinal.go

--- its 350/Assignment-2/Assignmentfinal.go
+++ its 350/Assignment-2/Assignmentfinal.go
@@ -2,12 +2,12 @@
 Karo Rasool kk19046@auis.edu.krd*/
 
 /*
-Requirement 1: T(n) = 9n^2 + 9n + 21, O(n) = n^2
-Requirement 2 T(n) = 8, O(n) = 1
-Requirement 3 T(n) = 9, O(n) = 1
-Requirement 5 T(n) = 17n^2 + 27n + 29, O(n) = n^2
-Requirement 6 T(n) = 81n + 71, O(n) = n
-Requirement 7 T(n) = 72n + 28, O(n) = n
+Requirement 1: T(n) = 9n^2 + 9n + 21, O(n^2)
+Requirement 2 T(n) = 8, O(1)
+Requirement 3 T(n) = 9, O(1)
+Requirement 5 T(n) = 17n^2 + 27n + 29, O(n^2)
+Requirement 6 T(n) = 81n + 71, O(n)
+Requirement 7 T(n) = 72n + 28, O(n)
 */
 package main
 
