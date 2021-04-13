4334742332da1657ca57edc7f6a14e8b35f8621d
karolord <karokodo123@gmail.com>
2021-04-13 21:15:31 +0300

    commit

--- SE421/Assignment2/Artist.java
+++ SE421/Assignment2/Artist.java
@@ -3,11 +3,4 @@ package SE421.Assignment2;
 public class artist {
     private String artistname;
 
-        return this.artistName;
-    }
-
-    public void setArtistName(String artistName) {
-        this.artistName = artistName;
-    }
-
 }
