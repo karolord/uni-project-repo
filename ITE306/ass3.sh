f71d7ac5c0fb57a532155793da6294a65f29a7fa
karolord <karokodo123@gmail.com>
2021-04-26 18:44:55 +0300

    commit

--- ITE306/ass3.sh
+++ ITE306/ass3.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-username1="$RANDOM
-username2=“$RANDOM”
-username3=“$RANDOM”
+username1=“user-$RANDOM”
+username2=“user-$RANDOM”
+username3=“user-$RANDOM”
 
-echo “$username3”
-echo “$username2”
-echo “$username1”
+useradd -m $username1
+useradd -m $username2
+useradd -m $username3
 
+echo -e "$x\n$x"| passwd  $username1
+echo -e "$x\n$x"| passwd  $username2
+echo -e "$x\n$x"| passwd  $username3
+passwd -x 14 $username1
+passwd -x 14 $username2
+passwd -x 14 $username3
