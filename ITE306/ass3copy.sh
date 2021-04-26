8a4dd60727d15d6d2fc92265cac47961491a9883
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-04-26 18:00:21 +0300

    Update ass3copy.sh

--- ITE306/ass3copy.sh
+++ ITE306/ass3copy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#Requirement 1
 username1=“$RANDOM”
 username2=“$RANDOM”
 username3=“$RANDOM”
@@ -6,20 +7,26 @@ useradd -m user-“$username1”
 useradd -m user-“$username2”
 useradd -m user-“$username3”
 
-
+#Requirement 2
 useradd -m -g ite306 auis_test
 
+#Requirement 3
 echo “$x” | passwd “$username1”
 echo “$x” | passwd “$username2”
 echo “$x” | passwd “$username3”
+$username1 -M 14
+$username2 -M 14
+$username3 -M 14
 
-
+#Requirement 4
 top -U auis_test
 ps -u auis_test
 
-
+#Requirement 5
 cd /tmp/auis
 mv *.config /home/kn18-00011/Desktop
 
-
+#Requirement 6
 chmod -wx firefox
+
+#Requirement 7
