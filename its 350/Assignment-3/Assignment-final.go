723cd39216ba354788913196464f7758ce8ff703
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-04-28 00:51:20 +0300

    Update Assignment-final.go

--- its 350/Assignment-3/Assignment-final.go
+++ its 350/Assignment-3/Assignment-final.go
@@ -2,7 +2,7 @@
 Karo K. Rasool kk19046@auis.edu.krd*/
 /*
 Requirement 5: T(n) = 16n + 16, Big O = O(n)
-Requirement 6: T(n) = , Big O = O()
+Requirement 6: T(n) = 21n^3 + 49n^2 + 45n + 39, Big O = O(n^3)
 */
 /*
 Citations:
@@ -24,27 +24,27 @@ type Hashtable struct {
 }
 
 func (h *Hashtable) Inserthash(value string) {
-	index := hashfunction(value, len(h.hmap))
-	h.hmap[index].insertsorted(value)
+	index := hashfunction(value, len(h.hmap)) // 6
+	h.hmap[index].insertsorted(value) // 3
 }
 func hashfunction(key string, length int) int {
 	sum := int(key[len(key)-2])
 	return (sum - 19) % length
 }
 func Initalizemap(size int) *Hashtable {
-	result := &Hashtable{}
-	result.hmap = make([]*Linkedlist, size)
-	for i := range result.hmap {
-		result.hmap[i] = &Linkedlist{}
+	result := &Hashtable{} // 3
+	result.hmap = make([]*Linkedlist, size) // 4
+	for i := range result.hmap { // 3n
+		result.hmap[i] = &Linkedlist{} // 3n
 	}
-	return result
+	return result // 1
 }
 func (h *Hashtable) Printhashtable() {
-	for i := 0; i < len(h.hmap); i++ {
-		node := h.hmap[i].head
-		for node != nil {
-			fmt.Printf("%s ", node.name)
-			node = node.next
+	for i := 0; i < len(h.hmap); i++ { // 2, 4n + 4, 3n
+		node := h.hmap[i].head // 4n
+		for node != nil { // 2n^2
+			fmt.Printf("%s ", node.name) // 2n^2
+			node = node.next // 2n^2
 		}
 	}
 }
@@ -69,25 +69,25 @@ func (l *Linkedlist) insertLinkedlist(value string) {
 
 //sorting for the hashtable
 func (l *Linkedlist) insertsorted(value string) {
-	newNode := &Node{name: value}
-	if l.head == nil {
-		newNode.next = l.head
-		l.head = newNode
-	} else if newNode.name[len(newNode.name)-1] <= l.head.name[len(l.head.name)-1] {
-		newNode.next = l.head
-		l.head = newNode
+	newNode := &Node{name: value} // 5
+	if l.head == nil { // 2
+		newNode.next = l.head // 2
+		l.head = newNode // 2
+	} else if newNode.name[len(newNode.name)-1] <= l.head.name[len(l.head.name)-1] { // 9
+		newNode.next = l.head // 2
+		l.head = newNode // 2
 	} else {
-		tmp := l.head
-		for tmp != nil {
-			if tmp.next == nil {
-				tmp.next = newNode
-				break
-			} else if newNode.name[len(newNode.name)-1] < tmp.next.name[len(tmp.next.name)-1] {
-				newNode.next = tmp.next
-				tmp.next = newNode
-				break
+		tmp := l.head // 3
+		for tmp != nil { // 2n
+			if tmp.next == nil { // 2n
+				tmp.next = newNode // 2n
+				break // 0
+			} else if newNode.name[len(newNode.name)-1] < tmp.next.name[len(tmp.next.name)-1] { // 9n
+				newNode.next = tmp.next // 2n
+				tmp.next = newNode // 2n
+				break // 0
 			}
-			tmp = tmp.next
+			tmp = tmp.next // 2n
 		}
 	}
 }
@@ -163,15 +163,15 @@ func (hashtable *Hashtable) requirement6() {
 		return // 0
 	}
 	fmt.Println("Resized hashtable: ") // 1
-	table6 := Initalizemap(n / 3) // 4 + 
+	table6 := Initalizemap(n / 3) // 4 + 6n + 8
 	for i := 0; i < len(hashtable.hmap); i++ { // 2, 4n + 4, 3n
 		node := hashtable.hmap[i].head // 4n
 		for node != nil { // 2n^2
-			table6.Inserthash(node.name) // 2n^2 + 
+			table6.Inserthash(node.name) // 2n^2 + 9n^2 + 21n^3 + 27n^2
 			node = node.next // 2n^2
 		}
 	}
-	table6.Printhashtable() // 1 +
+	table6.Printhashtable() // 1 + 17n + 12 
 }
 func main() {
 	tablereq4 := Initalizemap(26)
