f63b73fa42c19ef0b760b3a2d47be1290438e71d
karolord <karokodo123@gmail.com>
2021-05-03 15:09:32 +0300

    commit

--- SE421/Assignment3/EmailAddresses.java
+++ SE421/Assignment3/EmailAddresses.java
@@ -1,9 +1,14 @@
+
 import java.util.*;
 
-public class EmailAddresses implements Em {
-    private String OwnerAddress;
+public class EmailAddresses {
     private LinkedList<Messages> Sent = new LinkedList<Messages>();
     private LinkedList<Messages> Received = new LinkedList<Messages>();
+    private String OwnerAddress;
+
+    public String getOwnerAddress() {
+        return this.OwnerAddress;
+    }
 
     public void setReceived(Messages msg) {
         this.Received.add(msg);
--- SE421/Assignment3/Ui.java
+++ SE421/Assignment3/Ui.java
@@ -1,13 +1,44 @@
+import java.util.*;
+
 public class Ui {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ComposeMessage();
+    }
+
     public static void startmenu() {
         System.out.println("Welcome to the greatest email client application");
         System.out.println("Enter your email address: ");
     }
 
-    public static void ComposeMessage(){
-        System.out.println("Pl\");
-
-
+    public static void ComposeMessage() {
+        System.out.println("Please enter the message Title");
+        String title = scanner.nextLine();
+        System.out.println("Please enter the message Description");
+        String Description = scanner.nextLine();
+        LinkedList<EmailAddresses> SentTo = new LinkedList<EmailAddresses>();
+        LinkedList<EmailAddresses> CC = new LinkedList<EmailAddresses>();
+        while (true) {
+            System.out.println(
+                    "Please enter the emails you want to sent the message to one at a time:( or 0 to exit the loop)");
+            String email = scanner.next();
+            if (email.equals("0"))
+                break;
+            EmailAddresses emailAddress = Factory.GetEmail(email);
+            boolean x = true;
+            for (Iterator i = main.allEmails.iterator(); i.hasNext();) {
+                EmailAddresses tmp = (EmailAddresses) i.next();
+                if (email.equals((tmp.getOwnerAddress()))) {
+                    SentTo.add(tmp);
+                    x = false;
+                }
+            }
+            if (x) {
+                SentTo.add(emailAddress);
+                main.allEmails.add(emailAddress);
+            }
+        }
 
     }
 }
