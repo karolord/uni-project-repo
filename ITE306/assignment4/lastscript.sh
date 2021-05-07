70d83a13cb330840ba590960d75d7124a0c5fd3e
karolord <karokodo123@gmail.com>
2021-05-08 01:38:05 +0300

    commit

--- ITE306/assignment4/lastscript.sh
+++ ITE306/assignment4/lastscript.sh
@@ -8,7 +8,7 @@ lxc exec Webserver -- sudo systemctl enable apache2
 lxc exec Webserver pidof apache2
 lxc exec Webserver -- sudo renice -n 19 $(lxc exec Webserver pidof apache2)
 #requirement 4 
-lxc exec Webserver apt install maillutils
+lxc exec Webserver apt install mailutils
 string1='#!/bin/bash
 sudo renice -n 19 $(pidof apache2)
 echo "hello" | mail -s "notification" kk19046@auis.edu.krd
@@ -16,7 +16,7 @@ echo "hello" | mail -s "notification" kn18011@auis.edu.krd'
 string2='[Unit]
 Description="this will change the priority of apache2 and notify the users"
 [Service]
-Execstart=/etc/systemd/system/script.sh'
+ExecStart=/etc/systemd/system/script.sh'
 echo "$string1" >> script.sh
 echo "$string2" >> email-apache.service
 lxc file push script.sh Webserver/etc/systemd/system/
