c6e565bfedb79c5b1839e90bf4815e96657c7825
karolord <karokodo123@gmail.com>
2021-05-07 18:04:50 +0300

    commit

---
+++ ITE306/assignment4/assignment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Requirement 1
+lxc launch image:ubuntu/21.04 Webserver
+#Requirement 2
+lxc exec Webserver apt install apache2
+lxc exec Webserver -- sudo systemctl enable apache2
+#Requirement 3
+lxc exec Webserver pidof apache2
+lxc exec Webserver -- sudo renice -n 19 $(lxc exec Webserver pidof apache2)
+#requirement4 
+
+
+
