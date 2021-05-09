fa35090fe4d346c5f7ae44127bec9d41edfa8987
karolord <karokodo123@gmail.com>
2021-05-09 19:57:04 +0300

    COMMIT

--- its 350/Assignment-4/assignment.go
+++ its 350/Assignment-4/assignment.go
@@ -1,5 +1,45 @@
 package main
 
+type Student struct {
+	Name    string
+	ID      int
+	Address string
+}
+
+type GeneralTree struct {
+	value  *Student
+	Right  *GeneralTree
+	Left   *GeneralTree
+	Middle *GeneralTree
+}
+
+func (g *GeneralTree) InsertGeneralTree(S *Student) {
+	if g.value == nil {
+		g.value = S
+		return
+	}
+	if S.Name[0] < g.value.Name[0] {
+		if g.Left == nil {
+			g.Left = &GeneralTree{value: S}
+		} else {
+			g.Left.InsertGeneralTree(S)
+		}
+	} else if S.Name[0] > g.value.Name[0] {
+		if g.Right == nil {
+			g.Right = &GeneralTree{value: S}
+		} else {
+			g.Right.InsertGeneralTree(S)
+		}
+	} else if S.Name[0] == g.value.Name[0] {
+		if g.Middle == nil {
+			g.Middle = &GeneralTree{value: S}
+		} else {
+			g.Middle.InsertGeneralTree(S)
+		}
+	}
+
+}
+
 func main() {
 
 }
