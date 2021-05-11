0e328f11225219c592f8426de023400a557bd92f
karolord <karokodo123@gmail.com>
2021-05-11 20:52:40 +0300

    commit

--- Assignment4/TestMessages.java
+++ Assignment4/TestMessages.java
@@ -1,17 +1,32 @@
+import java.util.*;
+
 public class TestMessages {
   public void TestgetSentTo() {
     //
+    AddressInterface fakeAddress = new AddressInterface() {
+      public void method() {
+      }
+    };
+    LinkedList<AddressInterface> fakelist = new LinkedList<AddressInterface>();
+    fakelist.add(fakeAddress);
+
+    Messages m = new Messages("title", "Discription", fakelist, fakelist);
+    m.getSentTo();
   }
+
   public void TestgetCC() {
     //
   }
+
   public void TestgetTitle() {
     //
   }
+
   public void TestgetDescription() {
     //
   }
-  public void TestMessages() {
+
+  public void TestMessage() {
     //
   }
 }
