25bb1acc02d46973f50b4b96df6afb1a08d6b31d
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-11 18:34:06 +0300

    Create Test.java

---
+++ Assignment4/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+  //choose class to test
+  //remove tightly coupledness
+  //Use polymorphism (interfaces)
+  //Use either dependency injection or factory design pattern
+  //if factory design: 1. create fake objects in place of objects that are tightly coupled
+}
