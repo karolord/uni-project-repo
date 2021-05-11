15e1462c607d96b3f3b1d9b8c4e32ed2a1a9748d
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-11 20:21:17 +0300

    Update main.java

--- Assignment4/main.java
+++ Assignment4/main.java
@@ -1,9 +1,6 @@
 import java.util.*;
 
 public class main {
-    public static LinkedList<MessageInterface> allMessages = new LinkedList<MessageInterface>();
-    public static LinkedList<AddressInterface> allEmails = new LinkedList<AddressInterface>();
-    public static EmailAddresses CurrentEmail;
     
     public static void main(String[] args) {
         Ui.startmenu();
