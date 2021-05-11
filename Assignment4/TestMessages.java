9cbcac62065700b3a6161fa7d7e0d9a3b45481c7
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-11 20:26:06 +0300

    Update and rename Test.java to TestMessages.java

--- Assignment4/TestMessages.java
+++ Assignment4/TestMessages.java
@@ -1,7 +1,22 @@
-public class Test {
+public class TestMessages {
   //choose class to test
   //remove tightly coupledness
   //Use polymorphism (interfaces)
   //Use either dependency injection or factory design pattern
   //if factory design: 1. create fake objects in place of objects that are tightly coupled
+  public void TestgetSentTo() {
+    //
+  }
+  public void TestgetCC() {
+    //
+  }
+  public void TestgetTitle() {
+    //
+  }
+  public void TestgetDescription() {
+    //
+  }
+  public void TestMessages() {
+    //
+  }
 }
