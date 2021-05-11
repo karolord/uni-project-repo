98f5b61224668289559201d0791e1a6bab51f7e0
karolord <karokodo123@gmail.com>
2021-05-11 20:23:01 +0300

    commit

--- Assignment4/Globals.java
+++ Assignment4/Globals.java
@@ -10,15 +10,4 @@ public class Globals {
   // public class Ui
   public static Scanner scanner = new Scanner(System.in);
 
-  // public class Messages
-  public static final String Title;
-  public static final String Description;
-  public static final LinkedList<AddressInterface> SentTo = new LinkedList<AddressInterface>();
-  public static final LinkedList<AddressInterface> CC = new LinkedList<AddressInterface>();
-
-  // public class EmailAddresses
-  public static String OwnerAddress;
-  public static LinkedList<MessageInterface> Sent = new LinkedList<MessageInterface>();
-  public static LinkedList<MessageInterface> Received = new LinkedList<MessageInterface>();
-
 }
--- Assignment4/Ui.java
+++ Assignment4/Ui.java
@@ -1,24 +1,23 @@
 import java.util.*;
 
 public class Ui {
-    public static Scanner scanner = new Scanner(System.in);
 
     public static void startmenu() {
         System.out.println("Welcome to the greatest email client application");
         System.out.println("Enter your email address: ");
-        String Current = scanner.nextLine();
-        main.CurrentEmail = new EmailAddresses(Current);
+        String Current = Globals.scanner.nextLine();
+        Globals.CurrentEmail = new EmailAddresses(Current);
         testemails();
         PrintInbox();
-        main.allEmails.add(main.CurrentEmail);
+        Globals.allEmails.add(Globals.CurrentEmail);
         while (true) {
             System.out.println("Please select one of the following");
             System.out.println("1.Compose an email");
             System.out.println("2.Print sent directory");
             System.out.println("3.Print recieved directory");
             System.out.println("4.Exit");
-            int selection = scanner.nextInt();
-            scanner.nextLine();
+            int selection = Globals.scanner.nextInt();
+            Globals.scanner.nextLine();
             switch (selection) {
                 case 1:
                     ComposeMessage();
@@ -36,21 +35,21 @@ public class Ui {
     }
 
     public static void testemails() {
-        main.allEmails.add(new EmailAddresses("Karo@auis.com"));
-        main.allEmails.add(new EmailAddresses("Kosar@auis.com"));
-        Messages tmp = new Messages("Test email 1", "This email is a test", main.allEmails, main.allEmails);
-        main.CurrentEmail.setReceived(tmp);
-        main.allMessages.add(tmp);
-        tmp = new Messages("Test email 2", "This email is a test", main.allEmails, main.allEmails);
-        main.CurrentEmail.setReceived(tmp);
-        main.allMessages.add(tmp);
-        tmp = new Messages("Test email 3", "This email is a test", main.allEmails, main.allEmails);
-        main.CurrentEmail.setReceived(tmp);
-        main.allMessages.add(tmp);
+        Globals.allEmails.add(new EmailAddresses("Karo@auis.com"));
+        Globals.allEmails.add(new EmailAddresses("Kosar@auis.com"));
+        Messages tmp = new Messages("Test email 1", "This email is a test", Globals.allEmails, Globals.allEmails);
+        Globals.CurrentEmail.setReceived(tmp);
+        Globals.allMessages.add(tmp);
+        tmp = new Messages("Test email 2", "This email is a test", Globals.allEmails, Globals.allEmails);
+        Globals.CurrentEmail.setReceived(tmp);
+        Globals.allMessages.add(tmp);
+        tmp = new Messages("Test email 3", "This email is a test", Globals.allEmails, Globals.allEmails);
+        Globals.CurrentEmail.setReceived(tmp);
+        Globals.allMessages.add(tmp);
     }
 
     public static void PrintInbox() {
-        LinkedList<MessageInterface> inbox = main.CurrentEmail.getReceived();
+        LinkedList<MessageInterface> inbox = Globals.CurrentEmail.getReceived();
         System.out.println("Here are the messages available in your inbox");
         for (Iterator i = inbox.iterator(); i.hasNext();) {
             Messages tmpmsg = (Messages) i.next();
@@ -62,7 +61,7 @@ public class Ui {
     }
 
     public static void PrintSentdirectory() {
-        LinkedList<MessageInterface> inbox = main.CurrentEmail.getSent();
+        LinkedList<MessageInterface> inbox = Globals.CurrentEmail.getSent();
         System.out.println("Here are the messages available in your sent directory");
         for (Iterator i = inbox.iterator(); i.hasNext();) {
             Messages tmpmsg = (Messages) i.next();
@@ -91,29 +90,29 @@ public class Ui {
 
     public static void ComposeMessage() {
         System.out.println("Please enter the message Title:");
-        String title = scanner.nextLine();
+        String title = Globals.scanner.nextLine();
         System.out.println("Please enter the message Description:");
-        String Description = scanner.nextLine();
+        String Description = Globals.scanner.nextLine();
         System.out.println("Send to:");
         LinkedList<AddressInterface> SentTo = addEmails();
         System.out.println("CC:");
         LinkedList<AddressInterface> CC = addEmails();
         System.out.println("end");
         Messages message = new Messages(title, Description, SentTo, CC);
-        main.allMessages.add(message);
-        main.CurrentEmail.setSent(message);
+        Globals.allMessages.add(message);
+        Globals.CurrentEmail.setSent(message);
     }
 
     public static LinkedList<AddressInterface> addEmails() {
         LinkedList<AddressInterface> tmplink = new LinkedList<AddressInterface>();
         while (true) {
             System.out.println("Please enter the emails you want to add one at a time:( or 0 to exit the loop)");
-            String email = scanner.next();
+            String email = Globals.scanner.next();
             if (email.equals("0"))
                 break;
             EmailAddresses emailAddress = new EmailAddresses(email);
             boolean x = true;
-            for (Iterator i = main.allEmails.iterator(); i.hasNext();) {
+            for (Iterator i = Globals.allEmails.iterator(); i.hasNext();) {
                 EmailAddresses tmp = (EmailAddresses) i.next();
                 if (email.equals((tmp.getOwnerAddress()))) {
                     tmplink.add(tmp);
@@ -122,7 +121,7 @@ public class Ui {
             }
             if (x) {
                 tmplink.add(emailAddress);
-                main.allEmails.add(emailAddress);
+                Globals.allEmails.add(emailAddress);
             }
         }
         return tmplink;
