555d03f9ea3b62f92809690d9e33edd045c91cdd
itsKosarAz <57855107+itsKosarAz@users.noreply.github.com>
2021-05-11 20:27:10 +0300

    Update TestMessages.java

--- Assignment4/TestMessages.java
+++ Assignment4/TestMessages.java
@@ -1,8 +1,4 @@
 public class TestMessages {
-  //remove tightly coupledness
-  //Use polymorphism (interfaces)
-  //Use either dependency injection or factory design pattern
-  //if factory design: 1. create fake objects in place of objects that are tightly coupled
   public void TestgetSentTo() {
     //
   }
