89250c6efde4420af778c77fb43e1a826a3eb6bc
karolord <karokodo123@gmail.com>
2021-05-14 02:17:44 +0300

    commit

---
+++ Gui/src/AccessDB.java
@@ -0,0 +1,6 @@
+public class AccessDB {
+    final String DATABASE_URL = "jdbc:mysql://localhost:3306/airlinereservationdb";    
+    final String SELECT_QUERY =
+"SELECT * FROM airlinereservationdb.seat;";
+}
+
