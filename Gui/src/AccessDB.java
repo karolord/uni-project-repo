6dcbd5207d60c41f22b63c911bd0f087877472f9
karolord <karokodo123@gmail.com>
2021-05-14 09:50:10 +0300

    commit all

--- Gui/src/AccessDB.java
+++ Gui/src/AccessDB.java
@@ -1,6 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class AccessDB {
-    final String DATABASE_URL = "jdbc:mysql://localhost:3306/airlinereservationdb";    
-    final String SELECT_QUERY =
-"SELECT * FROM airlinereservationdb.seat;";
+    public static void main(String[] args) {
+        try
+        {
+            String x = "1A";
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/airlinereservationdb","root","karolord");
+            System.out.println("connected");
+            Statement st = con.createStatement();
+            st.executeUpdate("UPDATE airlinereservationdb.seat SET s_Reserved = 0 WHERE s_number ='"+x+"';" );
+        }
+        catch(SQLException ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
 
